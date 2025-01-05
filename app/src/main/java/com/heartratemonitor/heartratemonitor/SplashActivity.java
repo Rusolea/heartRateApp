776007866ebcdb537931fbbc9251fbd474021981
Osvaldo Juan Olea <osvaldo_olea_85@hotmail.com>
2025-01-04 21:27:23 -0300

    Add Splash

---
+++ app/src/main/java/com/heartratemonitor/heartratemonitor/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.heartratemonitor.heartratemonitor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Redirigir a MainActivity después del delay
+//        ContextCompat.getMainExecutor(this).execute(() -> {
+//            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+//            startActivity(intent);
+//            finish(); // Finaliza la actividad del Splash para que no regrese
+//        });
+
+        // Retraso de 3 segundos (3000 milisegundos)
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Finaliza la actividad del Splash para que no regrese
+
+        }, 3000);
+
+
+
+    }
+}
